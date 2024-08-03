f4831115c3f846090d74f5421d4ccb220531ae3e
chryzcode <alabaolanrewaju13@gmail.com>
2024-08-03 13:57:02 +0100

    push

---
+++ app/utils/AuthContext.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { createContext, useReducer, useContext, useEffect } from "react";
+
+const AuthStateContext = createContext();
+const AuthDispatchContext = createContext();
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      localStorage.setItem("user", JSON.stringify(action.payload));
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case "LOGOUT":
+      localStorage.removeItem("user");
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
+};
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, { user: null });
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
+
+  return (
+    <AuthStateContext.Provider value={state}>
+      <AuthDispatchContext.Provider value={dispatch}>{children}</AuthDispatchContext.Provider>
+    </AuthStateContext.Provider>
+  );
+};
+
+export const useAuthState = () => useContext(AuthStateContext);
+export const useAuthDispatch = () => useContext(AuthDispatchContext);
